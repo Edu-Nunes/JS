3dd1bc4911e21fcbbeb55565ce8ab6b0ef175bfb
Edu-Nunes <enunes6547@gmail.com>
2024-07-20 17:47:33 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName('inicial')[0].innerHTML=('teste');
+document.getElementById('texto').innerHTML ="Este é um txto js";
+document.getElementsByTagName('b')[0].innerHTML =
